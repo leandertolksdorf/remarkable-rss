caf8294af55dfa6ebce9a0effec7be98a3fa45c9
Leander Tolksdorf <leander.tolksdorf@fu-berlin.de>
2022-01-07 14:05:26 +0100

    refactor mongoose connection

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,5 +1,4 @@
-import mongoose, { connect, ConnectOptions } from "mongoose";
-import { NextApiRequest, NextApiResponse } from "next";
+import { connect, ConnectOptions } from "mongoose";
 import config from "./config";
 
 const MONGO_URL = config.MONGO_URL;
@@ -8,18 +7,24 @@ if (!MONGO_URL) {
   throw new Error("MONGO_URL must be defined in environment");
 }
 
-const options: ConnectOptions = {};
-
-export const withDatabase = (
-  handler: (req: NextApiRequest, res: NextApiResponse) => void
-) => {
-  return async function (req: NextApiRequest, res: NextApiResponse) {
-    if (mongoose.connection.readyState === 0) {
-      await connect(MONGO_URL, options);
-    }
-
-    await handler(req, res);
-
-    mongoose.connection.close();
-  };
+const options: ConnectOptions = {
+  autoIndex: true,
+  autoCreate: true,
 };
+
+let cachedMongooseConnection = (global as any).mongoose;
+
+if (!cachedMongooseConnection) {
+  cachedMongooseConnection = (global as any).mongoose = null;
+}
+
+async function connectToDatabase() {
+  if (cachedMongooseConnection) {
+    return cachedMongooseConnection;
+  }
+
+  cachedMongooseConnection = await connect(MONGO_URL, options);
+  return cachedMongooseConnection;
+}
+
+export default connectToDatabase;
