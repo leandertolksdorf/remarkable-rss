36a8d7d7da58ba471472bcc3f1d98205db71de6a
Leander Tolksdorf <leander.tolksdorf@fu-berlin.de>
2022-01-07 14:06:09 +0100

    Close #4 Implement user routes

--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -1,46 +1,55 @@
 import bcrypt from "bcrypt";
+import { serialize } from "cookie";
 import jwt from "jsonwebtoken";
 import _ from "lodash";
 import { NextApiRequest, NextApiResponse } from "next";
-import { object, string, ValidationError } from "yup";
+import { object, string } from "yup";
 import config from "../../../lib/config";
-import { withDatabase } from "../../../lib/mongoose";
-import { UserModel } from "../../../models/user";
+import UserModel from "../../../models/user";
+import buildRouteHandler, { handlers } from "../../../util/buildRouteHandler";
 
-const postBodySchema = object({
-  username: string().required(),
-  password: string().required(),
-});
+const handlers: handlers = {
+  GET: async (req: NextApiRequest, res: NextApiResponse) => {
+    const { cookies } = req;
+    const cookiesSchema = object({
+      token: string().required(),
+    });
+    const { token } = cookiesSchema.validateSync(cookies);
+    const decodedToken = jwt.verify(token, config.JWT_SECRET);
+    const user = await UserModel.findOne({
+      username: (<any>decodedToken).username as string,
+    });
+    if (!user) {
+      throw new Error("404");
+    }
+    const response = {
+      username: user.username,
+      hasDeviceToken: user.deviceToken !== null,
+      feeds: user.feeds,
+    };
+    return res.status(200).json(response);
+  },
+  POST: async (req: NextApiRequest, res: NextApiResponse) => {
+    const { body } = req;
+    const bodySchema = object({
+      username: string().required(),
+      password: string().required(),
+    });
+    const user = bodySchema.validateSync(body);
+    user.password = bcrypt.hashSync(user.password, 10);
+    const createdUser = await UserModel.create({ deviceToken: null, ...user });
+    const payload = _.pick(createdUser, "username");
+    const token = jwt.sign(payload, config.JWT_SECRET);
+    res.setHeader(
+      "Set-Cookie",
+      serialize("token", token, {
+        httpOnly: true,
+        secure: process.env.NOVE_ENV === "production",
+        path: "/",
+      })
+    );
+    return res.status(201).json({ success: true });
+  },
+};
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { body, method } = req;
-
-  switch (method) {
-    case "GET":
-      break;
-    case "POST":
-      try {
-        const user = postBodySchema.validateSync(body);
-        user.password = bcrypt.hashSync(user.password, 10);
-        const createdUser = await UserModel.create(user);
-        const payload = _.pick(createdUser, "username");
-        const token = jwt.sign(payload, config.JWT_SECRET);
-        return res
-          .status(201)
-          .json({ success: true, token: "Bearer " + token });
-      } catch (e: any) {
-        let status;
-        if (e instanceof ValidationError) {
-          status = 400;
-        } else if (e.code === 11000) {
-          status = 409;
-        } else {
-          status = 500;
-        }
-        return res.status(status).json({ success: false });
-      }
-      break;
-  }
-}
-
-export default withDatabase(handler);
+export default buildRouteHandler(handlers);
---
+++ pages/api/user/login/index.ts
@@ -0,0 +1,47 @@
+import bcrypt from "bcrypt";
+import { serialize } from "cookie";
+import jwt from "jsonwebtoken";
+import _ from "lodash";
+import { NextApiRequest, NextApiResponse } from "next";
+import { object, string } from "yup";
+import config from "../../../../lib/config";
+import UserModel from "../../../../models/user";
+import buildRouteHandler, {
+  handlers,
+} from "../../../../util/buildRouteHandler";
+
+const handlers: handlers = {
+  POST: async (req: NextApiRequest, res: NextApiResponse) => {
+    const { body } = req;
+    const bodySchema = object({
+      username: string().required(),
+      password: string().required(),
+    });
+    const { username, password } = bodySchema.validateSync(body);
+    const user = await UserModel.findOne({ username });
+
+    if (!user) {
+      throw new Error("404");
+    }
+
+    const isValidPassword = bcrypt.compareSync(password, user.password);
+
+    if (!isValidPassword) {
+      throw new Error("401");
+    }
+
+    const payload = _.pick(user, "username");
+    const token = jwt.sign(payload, config.JWT_SECRET);
+    res.setHeader(
+      "Set-Cookie",
+      serialize("token", token, {
+        httpOnly: true,
+        secure: process.env.NOVE_ENV === "production",
+        path: "/",
+      })
+    );
+    return res.status(201).json({ success: true });
+  },
+};
+
+export default buildRouteHandler(handlers);
