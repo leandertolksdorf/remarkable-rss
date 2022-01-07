129cb1c13a2b3b0024fb7b7815a50b2e8ec7220c
Leander Tolksdorf <leander.tolksdorf@fu-berlin.de>
2022-01-07 14:23:12 +0100

    #5 initialize tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
